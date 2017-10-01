9fa6c312890703265ca0e4f419d3e76b144a45c4
Morghen <remy.mauhin@gmail.com>
2017-10-01 14:50:27 +0200

    default

---
+++ ApplicationCheckIn.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+
+
+
+
+
+
+
+
+int main()
+{
+	cout << "*** IDENTIFICATION ***" << endl;
+	cout << "Login : ";
+	cin >> login;
+	cout << "Mot de passe : ";
+	cin >> password;
+	//Fct qui check le login-mdp (renvoie 0 si OK, renvoie -1 si erreur)
+	if()
+	
+
+
+
+
+
+}
+
+
