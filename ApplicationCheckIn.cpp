c5ed11c7a97acd77d9adb0514fa418208e4195ca
Morghen <remy.mauhin@gmail.com>
2017-10-01 14:59:58 +0200

    default

--- ApplicationCheckIn.cpp
+++ ApplicationCheckIn.cpp
@@ -4,24 +4,27 @@
 
 
 
-
-
+void Identify();
+int CheckLoginPassword(char* login,char* password);
 
 
 
 
 int main()
 {
-	cout << "*** IDENTIFICATION ***" << endl;
-	cout << "Login : ";
-	cin >> login;
-	cout << "Mot de passe : ";
-	cin >> password;
-	//Fct qui check le login-mdp (renvoie 0 si OK, renvoie -1 si erreur)
-	if()
-	
+	int loopAgain = 1;
+	while(loopAgain == 1)
+	{
+		cout << "*** IDENTIFICATION ***" << endl;
+		cout << "Login : ";
+		cin >> login;
+		cout << "Mot de passe : ";
+		cin >> password;
+		//Fct qui check le login-mdp (renvoie 0 si OK, renvoie -1 si erreur)
+		if(CheckLoginPassword==-1)
+			cout << "Compte inconnu, verifier votre login/mot de passe" << endl;
 
-
+	}
 
 
 
