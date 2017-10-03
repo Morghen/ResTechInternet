3f3ad72089252f3bb2ae9c372d7471564f966f4e
Morghen <remy.mauhin@gmail.com>
2017-10-03 10:22:34 +0200

    default

--- libUtils.cpp
+++ libUtils.cpp
@@ -94,30 +94,7 @@ int CheckLoginPassword(char* lg,char* password)
 	cout << "Fin fichier atteint, aucune correspondance" << endl;
 	fs.close();
 	return -1;
-}
-
-
-void Identify()
-{
-	// Variables
-	int loopAgain = 1;
-	char login[50]={0},password[50]={0};
-	// Recherche
-	while(loopAgain == 1)
-	{
-		cout << "*** IDENTIFICATION ***" << endl;
-		cout << "Login : ";
-		cin >> login;
-		cout << "Mot de passe : ";
-		cin >> password;	
-		if(CheckLoginPassword(login,password) == -1)	//Fct qui check le login-mdp (renvoie 0 si OK, renvoie -1 si erreur)
-			cout << "Compte inconnu, verifiez votre login/mot de passe" << endl;
-		else
-		{
-			cout << "Compte valide, bienvenue " << login << endl;
-			loopAgain=0;
-		}
-	}
+	return 1;
 }
 
 int sendMsgRequest(int phandle, TypeRequete pt, char *pmsg, int psize)
